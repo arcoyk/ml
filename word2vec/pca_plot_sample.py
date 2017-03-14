db59269140c68020f8a0ebea33c15f130f811e83
yui <yui@racoon.local>
2017-03-15 02:01:49 +0900

    word2vec pca plot

---
+++ word2vec/pca_plot_sample.py
@@ -0,0 +1,32 @@
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+from gensim.models import word2vec
+
+TEXT_DIR = 'abcde.txt'
+MODEL_DIR = 'abcde.model'
+
+# Train
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+sentences = word2vec.LineSentence(TEXT_DIR)
+model = word2vec.Word2Vec(sentences)
+model.save(MODEL_DIR)
+
+X = [model[w] for w in model.wv.index2word]
+T = [w for w in model.wv.index2word]
+
+# PCA
+X = np.array(X)
+pca = PCA(n_components=2)
+X = pca.fit_transform(X)
+
+# Plot
+plt.plot(X[:,0], X[:,1], 'x')
+for i in range(len(X)):
+    x = X[i][0]
+    y = X[i][1]
+    t = T[i]
+    plt.text(x, y, t)
+
+plt.show()
