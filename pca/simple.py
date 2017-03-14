61b9ed389771ae07773399f896a7b4721a71dff5
yui <yui@racoon.local>
2017-03-15 01:43:18 +0900

    pca with graph output

--- pca/simple.py
+++ pca/simple.py
@@ -1,10 +1,16 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 x = [random.random() for x in range(100)]
 y = [random.random() for y in range(100)]
 z = [z % 2 for z in range(100)]
 X = [[x[i], y[i], z[i]] for i in range(100)]
 
+X = np.array(X)
 pca = PCA(n_components=2)
+X = pca.fit_transform(X)
+
+plt.plot(X[:,0], X[:,1], 'x')
+plt.show()
