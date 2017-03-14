d24a5e16e178e191c0dbab720480d87d660718f0
yui <yui@racoon.local>
2017-03-15 01:48:05 +0900

    simple text

--- vis/simple.py
+++ vis/simple.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0, 10, 0.1)
+x = np.arange(-4, 4, 0.1)
 y = np.sin(x)
-plt.plot(x, y)
+plt.plot(x, y, '.')
+plt.text(1, 0, 'Hello World')
 plt.show()
+
