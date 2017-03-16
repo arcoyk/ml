d446361e92241288921a4c07e0928d0ae01e2dc4
yui <yui@racoon.local>
2017-03-17 04:41:50 +0900

    before nn layer

--- nn/with_vis.py
+++ nn/with_vis.py
@@ -39,31 +39,54 @@ def layer_calc(x, w):
 def define(x):
     return x[0]
 
-def train():
+def error_rate():
     d = 0
     for p in data:
         x = [p[0], p[1]]
         t = p[2]
         for wx in w:
             x1 = layer_calc(x, wx)
-        #x1 = layer_calc(x, w1)
-        #x1 = layer_calc(x, w2)
-        #x1 = layer_calc(x, w3)
         d += abs(define(x1) - t)
     error_rate = d / len(data)
     return error_rate
 
+def matrix(row_n, col_n):
+    mat = []
+    for y in range(col_n):
+        col = []
+        for x in range(row_n):
+            col.append(rand())
+        mat.append(col)
+    return mat
+
+def print_matrix(w):
+    for wx in w:
+        for wxx in wx:
+            print(wxx)
+        print('-------')
+
+def layer(nn):
+    w = []
+    for i in range(len(nn) - 1):
+        w.append(matrix(nn[i], nn[i + 1]))
+    return w
+
+"""
 w1 = []
 for i in range(2):
     w1.append([rand(), rand()])
 w2 = []
 for i in range(4):
     w2.append([rand(), rand()])
+
 w3 = []
 for i in range(1):
     w3.append([rand(), rand()])
 
 w = [w1, w2, w3]
+"""
+
+w = layer([2, 6, 5, 1])
 
 def get_slope(wx):
     slope = [[0 for y in x] for x in wx]
@@ -73,25 +96,23 @@ def get_slope(wx):
             inc = wx[xi][yi] + d
             dec = wx[xi][yi] - d
             wx[xi][yi] = inc
-            inc_e = train()
+            inc_e = error_rate()
             wx[xi][yi] = dec
-            dec_e = train()
+            dec_e = error_rate()
             dx = inc - dec
             dy = inc_e - dec_e
             slope[xi][yi] = dy / dx
             wx[xi][yi] = tmp
     return slope
 
+def train(d, alpha, epoch):
+    for i in range(epoch):
+        for i in range(len(w)):
+            slope = get_slope(w[i])
+            w[i] = [[w[i][xi][yi] - slope[xi][yi] * alpha for yi in range(len(w[i][xi]))] for xi in range(len(w[i]))]
+        print("E:", error_rate())
+
 d = 0.1
 alpha = 0.2
-for i in range(10):
-    #slope1 = get_slope(w1)
-    #slope2 = get_slope(w2)
-    #slope3 = get_slope(w3)
-    for i in range(len(w)):
-        slope = get_slope(w[i])
-        w[i] = [[w[i][xi][yi] - slope[xi][yi] * alpha for yi in range(len(w[i][xi]))] for xi in range(len(w[i]))]
-    #w1 = [[w1[xi][yi] - slope1[xi][yi] * alpha for yi in range(len(w1[xi]))] for xi in range(len(w1))]
-    #w2 = [[w2[xi][yi] - slope2[xi][yi] * alpha for yi in range(len(w2[xi]))] for xi in range(len(w2))]
-    #w3 = [[w3[xi][yi] - slope3[xi][yi] * alpha for yi in range(len(w3[xi]))] for xi in range(len(w3))]
-    print("E:", train())
+epoch = 10
+train(d, alpha, epoch)
