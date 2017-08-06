c891091420adcf0eebd3d3e65a042161534dc287
yui <yui@racoon.local>
2017-08-07 05:28:03 +0900

    doc2vec basic epoch

--- doc2vec/simple.py
+++ doc2vec/simple.py
@@ -1,27 +1,42 @@
+import os
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from subprocess import call
 from gensim.models.doc2vec import LabeledSentence
 from gensim import models
-from os import listdir
 
-def read_doc(path):
-    words = []
-    with open(path) as f:
-        words = f.read().split(' ')
-    name = path.split('/')[-1]
-    print(name)
-    return LabeledSentence(words = words, tags=[name])
+TRAIN = False
+DIR = 'sample/science/'
+MODEL = 'doc2vec.model'
 
-DIR = "./sample/science/"
-sentences = [read_doc(DIR + x) for x in listdir(DIR)]
-model = models.Doc2Vec(sentences)
-for epoch in range(20):
-    print('Epoch:', epoch + 1)
-    model.train(sentences)
-    model.alpha -= (0.025 - 0.0001) / 19
-    model.min_alpha = model.alpha
+def get_content(txt_path):
+    content = ''
+    with open(txt_path) as f:
+        content = f.read()
+    return content
 
-model.save("science.model")
-model = models.Doc2Vec.load("science.model")
+def read_docs():
+    rst = list()
+    for d in os.listdir(DIR):
+        if d.find('.txt') != -1:
+            content = get_content(DIR + d)
+            title = d
+            words = content.split(' ')
+            rst.append(LabeledSentence(words = words, tags=[title]))
+    return rst
 
-for tag in model.docvecs.doctags:
-    print(model.docvecs[tag])
+if TRAIN:
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    sentences = read_docs()
+    model = models.Doc2Vec(sentences)
+    for epoch in range(20):
+        model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)
+        model.alpha -= (0.025 - 0.0001) / 19
+        model.min_alpha = model.alpha
+    model.save(MODEL)
 
+model = models.Doc2Vec.load(MODEL)
+
+print(dir(model))
